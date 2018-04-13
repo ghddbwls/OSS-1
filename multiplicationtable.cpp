e17e66b68a76b8473369364cd43ede30405fff46
이상원 <lsw0833@hanmail.net>
2018-04-13 10:29:09 +0900

    Add 9times table

--- multiplicationtable.cpp
+++ multiplicationtable.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	int i, j;
-	for(i = 1;i < 9;i++)
+	for(i = 1;i <= 9;i++)
 	{
-		for(j = 1;j < 9;j++)
+		for(j = 1;j <= 9;j++)
 		{
 			cout << i << "*" << j << "=" << i*j << Endl;
 		}
